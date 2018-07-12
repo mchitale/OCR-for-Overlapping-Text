c3be2a7f50d5fd7623224cb6fbf74768b0205709
Maitreyi <35814905+mchitale@users.noreply.github.com>
2018-07-12 17:17:33 +0530

    Update keras_cnn_datagen.py

--- keras_cnn_datagen.py
+++ keras_cnn_datagen.py
@@ -2,43 +2,61 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-
+import keras
 
 model = Sequential()
-model.add(Conv2D(64, (3, 3), input_shape=(128,128,3)))
+model.add(Conv2D(32, (3, 3), input_shape=(64,64,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(128, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
+model.add(Dropout(0.25))
 # the model so far outputs 3D feature maps (height, width, features)
 
 model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
 model.add(Dense(128))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.25))
 model.add(Dense(62))
-model.add(Activation('sigmoid'))
+model.add(Activation('softmax'))
+
+rms = keras.optimizers.RMSprop(lr=0.0001, decay=1e-6)
 
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
 
-batch_size = 16
+batch_size = 32
 
 # this is the augmentation configuration we will use for training
 train_datagen = ImageDataGenerator(
         rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        zca_epsilon=1e-06,  # epsilon for ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        shear_range=0.,  # set range for random shear
+        zoom_range=0.,  # set range for random zoom
+        channel_shift_range=0.,  # set range for random channel shifts
+        fill_mode='nearest',  # set mode for filling points outside the input boundaries
+        cval=0.,  # value used for fill_mode = "constant"
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False,  # randomly flip images
+        preprocessing_function=None,  # set function that will be applied on each input
+        data_format=None,  # image data format, either "channels_first" or "channels_last"
+        validation_split=0.0)
 
 # this is the augmentation configuration we will use for testing:
 # only rescaling
@@ -49,22 +67,23 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 # batches of augmented image data
 train_generator = train_datagen.flow_from_directory(
         'C:/Users/machital/Desktop/OCR_Ovl/Eng_Seg/Train',  # this is the target directory
-        target_size=(128, 128),  # all images will be resized to 150x150
+        target_size=(64, 64),  # all images will be resized to 150x150
         batch_size=batch_size,
         class_mode='categorical')  # since we use binary_crossentropy loss, we need binary labels
 
 # this is a similar generator, for validation data
 validation_generator = test_datagen.flow_from_directory(
         'C:/Users/machital/Desktop/OCR_Ovl/Eng_Seg/Validation',
-        target_size=(128, 128),
+        target_size=(64, 64),
         batch_size=batch_size,
         class_mode='categorical')
 
 
 model.fit_generator(
         train_generator,
-        steps_per_epoch=2000 // batch_size,
-        epochs=25,
+        steps_per_epoch=5000 // batch_size,
+        epochs=100,
         validation_data=validation_generator,
         validation_steps=800 // batch_size)
+
 model.save_weights('first_try.h5')  # always save your weights after training or during training
